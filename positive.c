bc7819af3b0355416603b02cd6bc40894329cefe
arunmani20 <30829932+arunmani20@users.noreply.github.com>
2017-09-05 15:53:24 +0530

    Rename first.c to positive.c

---
+++ positive.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int n;
+printf("\n enter the number");
+scanf("%d",&n);
+if(n>0)
+printf("\n the given number is positive");
+elseif(n<0)
+printf("\nn the given number is negative");
+else
+printf("the given number is zero");
+}
