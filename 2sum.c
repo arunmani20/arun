b4c642977351d71357fe6aa87c9753f7fb5aeb8d
arunmani20 <30829932+arunmani20@users.noreply.github.com>
2018-01-21 11:32:01 +0530

    Rename sum.c to 2sum.c

---
+++ 2sum.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+	int n1,n2;
+	scanf("%d %d",&n1,&n2);
+	printf("\n the numbers are %d %d",n1,n2);
+	int result;
+	result=n1+n2;
+	printf("\n  sum of the numbers are %d",result);
+}
