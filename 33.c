fdcae691caa9737767b531d1d1b7b81e5472473a
arunmani20 <30829932+arunmani20@users.noreply.github.com>
2018-02-06 10:22:51 +0000

    Create 33.c

---
+++ 33.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+#include <string.h>
+void main()
+{
+    char s[200];
+int cou= 0, k;
+scanf("%[^\n]s", s);
+for (k = 0;s[k] != '\0';k++)
+{   if (s[k] == ' ')
+  cou++;
+   }
+ printf("number of words is %d ", cou);
+
+}
+
