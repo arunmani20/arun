adbd0b622eb373e2899fcdda74bf91db5f198b42
arunmani20 <30829932+arunmani20@users.noreply.github.com>
2018-02-22 16:52:42 +0530

    Create 107.c

---
+++ 107.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,c;
+    scanf("%d %d %d",&a,&b,&c);
+    printf("\n the values of a b c are %d %d %d",a,b,c);
+    printf("\n the value is %d",((a*b)/c));
+}
