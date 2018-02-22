b43a68b3fb451e263c59d5334d82e2a5f6e0198f
arunmani20 <30829932+arunmani20@users.noreply.github.com>
2018-02-22 16:22:33 +0530

    Create 113.c

---
+++ 113.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+    int n,k,i,a[23],cou=0;
+    scanf("%d %d",&n,&k);
+    printf("the values of n and k are %d %d",n,k);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    printf("\n the array values are");
+     for(i=0;i<n;i++)
+    {
+        printf("\n %d",a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(k==a[i])
+        {
+            cou++;
+        }
+    }
+    
+    printf("\nthe repition of k value is  %d",cou);
+    
+}
