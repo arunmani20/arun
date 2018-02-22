32029b662e619db93371896aea85a3dce7e2952e
arunmani20 <30829932+arunmani20@users.noreply.github.com>
2018-02-22 15:41:50 +0530

    Create 104.c

---
+++ 104.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+    int n,k;
+    scanf("%d %d",&n,&k);
+    printf("\n the numbers are %d %d",n,k);
+    int c;
+    c=pow(n,k);
+    printf("\n %d",c);
+}
