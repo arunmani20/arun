a6921d5c6dd060b088cf3c1f9dae0a52750331a4
arunmani20 <30829932+arunmani20@users.noreply.github.com>
2018-03-02 14:05:28 +0530

    Create 103.c

---
+++ 103.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+void main()
+{
+	char str[100];
+	int i;
+	printf("Enter a string: ");
+	scanf("%[^\n]s",str); 
+	for(i=0; str[i]!='\0'; i++)
+	{
+		if(i==0)
+		{
+			if((str[i]>='a' && str[i]<='z'))
+				str[i]=str[i]-32; 
+			continue;
+		}
+		if(str[i]==' ')
+		{
+
+			++i;
+			
+			if(str[i]>='a' && str[i]<='z')
+			{
+				str[i]=str[i]-32; 
+				continue; 
+			}
+		}
+		else
+		{
+			if(str[i]>='A' && str[i]<='Z')
+				str[i]=str[i]+32;
+		}
+	}
+	
+	printf("Capitalize string is: %s\n",str);
+	
+	
+}
